d34f630ff28b08139d01144d35854aea8d941d7c
Kirill <117843865+Kirill-Lyanoi@users.noreply.github.com>
2023-10-18 11:44:58 +0300

    first contribution

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,27 @@
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+"use strict"
+function solveEquation(a, b, c) {
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let x1, x2;
+    
+    if (d > 0) {
+      x1 = (- b + Math.sqrt(d)) / (2 * a);
+      x2 = (- b - Math.sqrt(d)) / (2 * a);
+      arr.push (x1, x2);
+    } else if (d === 0) {
+      x1 = -b / (2 * a); 
+      arr.push (x1);
+    }
+    return arr;
 }
+console.log(solveEquation(3, 2, 1));
 
-function summElementsWorker(...arr) {
-
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let p = ((percent / 100) / 12);
+  let s = amount - contribution;
+  let n = countMonths;
+  let mounthlyPayment = s * (p + (p / (((1 + p) ** n) - 1)));
+  let totalAmount = (mounthlyPayment * n).toFixed(2);
+  return Number(totalAmount);
 }
-
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+console.log(calculateTotalMortgage(10, 1000, 20000, 24));
